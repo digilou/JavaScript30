ac9a0e35bce8f6313c16744ee5a0b869c5b649e0
Amy Carney <digilou@users.noreply.github.com>
2017-04-12 11:50:46 -0800

    uploaded from codepen

---
+++ slide-in-on-scroll/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at http://codepen.io/digilou/pen/vmBrXq.
+
+ Project 13 of JS30
---
+++ slide-in-on-scroll/js/index.js
@@ -0,0 +1,38 @@
+// event runs too many times so you have to debounce
+// pass in a function to debounce
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(){
+  sliderImages.forEach(sliderImage => {
+    // how far down have they scrolled
+    // gives pixel level when it should be slide in when halfway
+    const slideInAt = (window.scrollY + window.innerHeight) - (sliderImage.height / 2);
+    // find bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    // don't scroll all the way past image
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if(isHalfShown && isNotScrolledPast){
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
